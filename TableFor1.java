7966385adf777f0718935493ef4f7628e91ebb8c
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-27 17:53:52 +0530

    Add files via upload

---
+++ TableFor1.java
@@ -0,0 +1,13 @@
+public class TableFor1
+{
+      public static void main(String[] args)
+	{ 
+	  // Print first 10 multiple of given number
+	   int num=5;
+	   System.out.println(num);
+	    for(int ctr=2;ctr<=10;ctr++)
+		{	
+		   System.out.println(num*ctr);
+		}	
+	}
+}

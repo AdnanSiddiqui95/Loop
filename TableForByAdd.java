00babaa264c374316e973e07a87d7370b18c1033
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-27 18:01:19 +0530

    Add files via upload

---
+++ TableForByAdd.java
@@ -0,0 +1,15 @@
+public class TableForByAdd
+{
+      public static void main(String[] args)
+	{ 
+	  // Print table by adding up numbers of given number
+	   int num=15;
+	   int sum=num;
+	   System.out.println(sum);
+	    for(int ctr=0;ctr<9;ctr++)
+		{	
+		   sum+=num;
+		   System.out.println(sum);
+		}	
+	}
+}

a5e5dd780973fa2f0d237a5ffab6c4bb9f0ea300
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-27 17:46:50 +0530

    Add files via upload

---
+++ SquareOf.java
@@ -0,0 +1,10 @@
+public class SquareOf
+{
+      public static void main(String[] args)
+	{  System.out.println(1);
+	   for(int ctr=2;ctr<10;ctr++)
+		{	
+		   System.out.println(ctr*ctr);
+		}	
+	}
+}

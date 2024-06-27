5ff7100f7e4d7213569e8d32bb7d22eabac90f98
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-27 17:50:51 +0530

    Update SquareOf.java

--- SquareOf.java
+++ SquareOf.java
@@ -1,8 +1,9 @@
 public class SquareOf
-{
+{	
+	//Square of first 10 number
       public static void main(String[] args)
 	{  System.out.println(1);
-	   for(int ctr=2;ctr<10;ctr++)
+	   for(int ctr=2;ctr<=10;ctr++)
 		{	
 		   System.out.println(ctr*ctr);
 		}	

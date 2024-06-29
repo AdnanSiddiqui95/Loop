949bc0b4f58f52459dd06002ce0015f0138417a0
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-29 10:24:09 +0530

    Add files via upload

---
+++ LoopDemoUsingDo.java
@@ -0,0 +1,15 @@
+
+public class LoopDemoUsingDo {
+
+	public static void main(String[] args) 
+	{
+		int ctr=0;
+		do
+		{
+			System.out.println("Adnan Siddiqui");
+			ctr++;
+		}while(ctr<5);
+	
+	}
+
+}

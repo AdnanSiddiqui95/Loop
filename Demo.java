665e513dd2d81c7b91669b7bce701c5d1d6bbc69
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-01 16:38:39 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo 
+{
+	// code to add num one by one from 1 to 10 and display all number
+
+	public static void main(String[] args)
+	{
+		int x=1;
+		for(int ctr=1;ctr<=10;ctr++)
+		{
+			System.out.println(x);
+			x=x+ctr;
+		}
+		System.out.println(x);
+
+
+	}
+
+}

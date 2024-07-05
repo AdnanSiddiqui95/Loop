abe6c8052c4a93172c2dfb2bdda5502c8214bc86
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-05 21:47:20 +0530

    Add files via upload

---
+++ Values1.java
@@ -0,0 +1,18 @@
+
+public class Values1 {
+
+	public static void main(String[] args) {
+		int x=1,y=10;
+		
+		while(x<=10)
+		{
+			System.out.println(x);
+			x++;
+			System.out.println(y);
+			y--;
+			
+		}
+
+	}
+
+}
